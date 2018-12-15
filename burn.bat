1842c3d4a1d70da49e4ea7ae60b0980c7df23e50
明明 <erickwok404@gmail.com>
2018-12-16 00:03:11 +0800

    Modified:   burn.bat   merge_av.bat
    Signed-off-by: 明明 <erickwok404@gmail.com>

--- burn.bat
+++ burn.bat
@@ -6,24 +6,27 @@ set TOP_RIGHT='main_w-overlay_w-10:10'
 set BUTTOM_LEFT='10:main_h-overlay_h-10'
 set BUTTOM_RIGHT='main_w-overlay_w-10:main_h-overlay_h-10'
 
-set Logo="E:\Pictures\Logo.ico"
+set Logo="D:\Pictures\logo.ico"
 set video=%1
 set ass=%~n2%~x2
 set output=%3
-set pos=%TOP_LEFT%
+set pos=%TOP_RIGHT%
 
 title burn ass and logo into video
 
 rem help manual
 if [%1]==[] (
+    echo;
 	echo Usage: burn.bat {video} {ass} {output}
-	echo The logo file is hard coded as "E:\Pictures\Logo.ico",
+	echo;
+	echo The logo file is now %Logo%,
 	echo if you want to change it, edit bat file manually.
 	echo The position of logo is top left corner, again, if you
 	echo want to change if, edit bat file manually.
+	echo;
 	exit
 )
 
 :burn
-ffmpeg.exe -i %video% -i %Logo% -filter_complex "[1]scale=200:200[logo];[0][logo]overlay=%pos%[taged];[taged]ass='%ass%'[sub]" -map [sub] -map 0:a %output%
+ffmpeg.exe -i %video% -i %Logo% -filter_complex "[1]scale=100:100[logo];[0][logo]overlay=%pos%[taged];[taged]ass='%ass%'[sub]" -map [sub] -map 0:a %output%
 exit
--- merge_av.bat
+++ merge_av.bat
@@ -1,14 +1,14 @@
 @echo off
 
 if [%1]==[] (
-	echo Usage: merge_av.bat {video} {audio} {output}
+	echo Usage: merge_av.bat {video} {audio}
 	exit
 )
 
 set video=%1
 set audio=%2
-set output=%3
+set output=out.mp4
 
 :merge
-ffmpeg -i %video% -i %audio% -c:v copy %output%
+ffmpeg -i %video% -i %audio% -c:v copy -c:a copy %output%
 exit
