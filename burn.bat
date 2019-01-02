1b338893c3b05c2c360da3a9ea69497759eed0a3
明明 <erickwok404@gmail.com>
2019-01-03 00:29:27 +0800

    新增了LoopbackExemptAll.bat
    Signed-off-by: 明明 <erickwok404@gmail.com>

--- burn.bat
+++ burn.bat
@@ -6,7 +6,7 @@ set TOP_RIGHT='main_w-overlay_w-10:10'
 set BUTTOM_LEFT='10:main_h-overlay_h-10'
 set BUTTOM_RIGHT='main_w-overlay_w-10:main_h-overlay_h-10'
 
-set Logo="D:\Pictures\logo.ico"
+set Logo="d:\Pictures\IMG_1721.PNG"
 set video=%1
 set ass=%~n2%~x2
 set output=%3
@@ -26,6 +26,9 @@ if [%1]==[] (
 	echo;
 	exit
 )
+
+:configure
+
 
 :burn
 ffmpeg.exe -i %video% -i %Logo% -filter_complex "[1]scale=100:100[logo];[0][logo]overlay=%pos%[taged];[taged]ass='%ass%'[sub]" -map [sub] -map 0:a %output%
