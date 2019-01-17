106d01e07f1a8262db057a0ffd8ba78da44eaffc
明明 <erickwok404@gmail.com>
2019-01-17 14:49:26 +0800

    修改了burn.bat
    Signed-off-by: 明明 <erickwok404@gmail.com>

--- burn.bat
+++ burn.bat
@@ -6,7 +6,7 @@ set TOP_RIGHT='main_w-overlay_w-10:10'
 set BUTTOM_LEFT='10:main_h-overlay_h-10'
 set BUTTOM_RIGHT='main_w-overlay_w-10:main_h-overlay_h-10'
 
-set Logo="d:\Pictures\IMG_1721.PNG"
+set Logo="d:\Pictures\Logo.ico"
 set video=%1
 set ass=%~n2%~x2
 set output=%3
@@ -31,5 +31,5 @@ if [%1]==[] (
 
 
 :burn
-ffmpeg.exe -i %video% -i %Logo% -filter_complex "[1]scale=100:100[logo];[0][logo]overlay=%pos%[taged];[taged]ass='%ass%'[sub]" -map [sub] -map 0:a %output%
+ffmpeg.exe -i %video% -i %Logo% -filter_complex "[1]scale=150:150[logo];[0][logo]overlay=%pos%[taged];[taged]ass='%ass%'[sub]" -map [sub] -map 0:a -crf 24 %output%
 exit
